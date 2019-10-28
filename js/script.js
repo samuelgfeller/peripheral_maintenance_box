55b5122643c437485a8b770fd1c098f2a560ae03
Samuel Gfeller <samuelgfeller@bluewin.ch>
2019-10-28 08:10:17 +0100

    refresh only if statuses changed [untested]

--- js/script.js
+++ js/script.js
@@ -2,27 +2,52 @@
 let success = ['up', 'ok'];
 let error = ['down', 'critical'];
 
+// Declare statusHash which is a string of the statuses and if it changes the dom is reloaded
+let newStatusHash = '';
+let oldStatusHash = '';
+
 let baseUrl = 'https://nagiosv1.rvo.one/nagios/cgi-bin/';
 
-let urlHostlist = baseUrl + 'statusjson.cgi?query=hostlist&formatoptions=enumerate';
+// Boolean true if its the first execution after page load. Changed to false after the use of it
+let firstExecution = true;
 
 // Call the function for the first time
-getHostlist();
+getHostlist(true);
 
 // Refresh the whole thing every 30 sec
 window.setInterval(function () {
+    let statusChanged = false;
 
-    // Empty the container to populate with refreshed data
-    $("#peripheralsContainer").empty();
+    if (firstExecution) {
+        oldStatusHash = newStatusHash;
+        firstExecution = false;
+    }
+    // console.log('old' + oldStatusHash, 'new' + newStatusHash);
 
+    if (oldStatusHash !== newStatusHash) {
+        oldStatusHash = newStatusHash;
+        statusChanged = true;
+
+        // Empty the container to populate with refreshed data
+        $("#peripheralsContainer").empty();
+    }
+    newStatusHash = '';
     // Call the function every 30 sec
-    getHostlist();
+    getHostlist(statusChanged);
+
 }, 30000);
 
-function getHostlist(){
+/**
+ * Get hostlist form nagios and engage future execution
+ *
+ * @param statusChanged boolean true if the status have changed and though the dom elements have to be updated
+ */
+function getHostlist(statusChanged) {
+
     // Get list of hosts with their statuses
+    let urlHostlist = baseUrl + 'statusjson.cgi?query=hostlist&formatoptions=enumerate';
     $.getJSON(urlHostlist, function (result) {
-        processHostlist(result['data']['hostlist']);
+        processHostlist(result['data']['hostlist'], firstExecution, statusChanged);
     });
 }
 
@@ -31,9 +56,10 @@ function getHostlist(){
  * and check if they are down. If its the case
  * searchError() function is called to investigate
  *
- * @param hostlist
+ * @param hostlist string
+ * @param statusChanged boolean true if the status have changed and though the dom elements have to be updated
  */
-function processHostlist(hostlist) {
+function processHostlist(hostlist, statusChanged) {
     // Declare i which is counted up and will be part of the hostId
     let i = 1;
 
@@ -64,14 +90,28 @@ function processHostlist(hostlist) {
         // Javascript runs asynchronously so the error is investigated and appended only after hosts got loaded in DOM
         if (isDown) {
 
-            // Add html to the top of the container because it's faulty and has to be displayed before all
-            $("#peripheralsContainer").prepend(html);
+            // Add 1 to hash to say that its down
+            // Added at first position because we prepend the element so it comes first at that time
+            newStatusHash = '1' + newStatusHash;
 
+            // If status changed
+            if (statusChanged) {
+                // Add html to the top of the container because it's faulty and has to be displayed before all
+                $("#peripheralsContainer").prepend(html);
+            }
             // Call function to search the error
-            searchError(hostname, domId);
+            searchError(hostname, domId, statusChanged);
         } else {
-            // Append host div with status (if error or not) to the container
-            $("#peripheralsContainer").append(html);
+
+            // Add 0 to hash to say that its up
+            newStatusHash += '0';
+
+            // If status changed
+            if (statusChanged) {
+
+                // Append host div with status (if error or not) to the container
+                $("#peripheralsContainer").append(html);
+            }
         }
 
         // Add 1 to the value of i
@@ -87,8 +127,9 @@ function processHostlist(hostlist) {
  *
  * @param host string hostname
  * @param domId string id of faulty host div
+ * @param statusChanged boolean true if the status have changed and though the dom elements have to be updated
  */
-function searchError(host, domId) {
+function searchError(host, domId, statusChanged) {
 
     // Get service list of host containing error
     let urlServiceList = baseUrl + 'statusjson.cgi?query=servicelist&formatoptions=enumerate&hostname=' + encodeURIComponent(host);
@@ -103,6 +144,12 @@ function searchError(host, domId) {
             // If the status of the service shows an error
             if (error.includes(status)) {
 
+                // Status on all services also added to the hash because if faulty service changes the page
+                // has to be updated too. The faulty host status nr is prepended to the hash and here it's after
+                // This is not relevant since it will be the same logic in every execution and to find if something
+                // changed it does the job
+                newStatusHash += '1';
+
                 // Get information about the faulty service
                 let urlServiceInfo = baseUrl + 'statusjson.cgi?query=service&hostname=' + encodeURIComponent(host) + '&servicedescription=' + encodeURIComponent(servicename);
                 $.getJSON(urlServiceInfo, function (result) {
@@ -110,14 +157,21 @@ function searchError(host, domId) {
                     // Declare serviceInfo with the information about the service
                     let serviceInfo = result['data']['service'];
 
-                    // Append the error message to the host div
-                    $('#' + domId).append(
-                        '<p class="errorMsg">' +
-                        'Service <b>' + serviceInfo['description'] + '</b> has following message: <b>' + serviceInfo['plugin_output'] + '</b>' +
-                        '</p>'
-                    );
+                    // If status changed
+                    if (statusChanged) {
+
+                        // Append the error message to the host div
+                        $('#' + domId).append(
+                            '<p class="errorMsg">' +
+                            'Service <b>' + serviceInfo['description'] + '</b> has following message: <b>' + serviceInfo['plugin_output'] + '</b>' +
+                            '</p>'
+                        );
+                    }
                 });
 
+            } else {
+                // Add 0 to say that service isn't faulty
+                newStatusHash += '0';
             }
         });
     });
